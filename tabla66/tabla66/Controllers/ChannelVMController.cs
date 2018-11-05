99328a416c2f45495e83ac40b7f0fa4c9f92336d
michaela s <michaela.scherman@gmail.com>
2018-11-05 20:00:25 +0100

    ViewModel och view

---
+++ tabla66/tabla66/Controllers/ChannelVMController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using tabla66.Models;
+using tabla66.ViewModel;
+
+namespace tabla66.Controllers
+{
+    public class ChannelVMController : Controller
+    {
+        // GET: ChannelVM
+        public ActionResult Index()
+        {
+            tablanEntities tb = new tablanEntities();
+            List<ChannelShowsVM> CSList = new List<ChannelShowsVM>();
+            var channelshowlist = (from s in tb.Show
+                                   join c in tb.Channel on s.Channel_id equals c.Id
+                                   select new { s.Title, c.Channel1, s.Info, s.Start_time, s.Duration }).ToList();
+
+            foreach (var v in CSList)
+            {
+                ChannelShowsVM item = new ChannelShowsVM();
+                item.Title = v.Title;
+                item.Channel1 = v.Channel1;
+                item.Info = v.Info;
+                item.Start_time = v.Start_time;
+                item.Duration = v.Duration;
+                CSList.Add(item);
+            }
+            
+            return View(CSList);
+        }
+    }
+}
---
+++ tabla66/tabla66/ViewModel/ChannelShowsVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace tabla66.ViewModel
+{
+    public class ChannelShowsVM
+    {
+
+        public string Channel1 { get; set; }
+        public string Genre1 { get; set; }
+        public string Title { get; set; }
+        public System.DateTime Start_time { get; set; }
+        public System.TimeSpan Duration { get; set; }
+        public string Info { get; set; }
+
+    }
+}
