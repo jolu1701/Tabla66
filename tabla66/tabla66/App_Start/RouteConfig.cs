5bbd48a556b079f7bc538426fb03d5e1205bbe4b
michaela s <michaela.scherman@gmail.com>
2018-11-14 16:35:09 +0100

    Tablåer på startsidan

--- tabla66/tabla66/App_Start/RouteConfig.cs
+++ tabla66/tabla66/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace tabla66
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Shows", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
